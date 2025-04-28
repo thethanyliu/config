f577881a7b627e8adeced1a3969299c528bdff2e
Ethan <ethanliu549@gmail.com>
2025-04-27 21:21:53 -0600

    changes

--- lua/plugins/lsp/lspconfig.lua
+++ lua/plugins/lsp/lspconfig.lua
@@ -95,9 +95,9 @@ return {
 		local capabilities = vim.lsp.protocol.make_client_capabilities()
 		capabilities = vim.tbl_deep_extend('force', capabilities, require('cmp_nvim_lsp').default_capabilities())
 		local servers = {
-			gopls = {},
 			pyright = {},
 			ts_ls = {},
+			gopls = {},
 			lua_ls = {
 				settings = {
 					Lua = {
--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -32,7 +32,7 @@ return {
 		local builtin = require 'telescope.builtin'
 		vim.keymap.set('n', '<leader>sh', builtin.help_tags, { desc = '[S]earch [H]elp' })
 		vim.keymap.set('n', '<leader>sk', builtin.keymaps, { desc = '[S]earch [K]eymaps' })
-		vim.keymap.set('n', '<leader>sf', builtin.find_files, { desc = '[S]earch [F]iles' })
+		vim.keymap.set('n', '<leader>sp', builtin.find_files, { desc = '[S]earch [F]iles' })
 		vim.keymap.set('n', '<leader>ss', builtin.builtin, { desc = '[S]earch [S]elect Telescope' })
 		vim.keymap.set('n', '<leader>sw', builtin.grep_string, { desc = '[S]earch current [W]ord' })
 		vim.keymap.set('n', '<leader>sg', builtin.live_grep, { desc = '[S]earch by [G]rep' })
@@ -40,6 +40,7 @@ return {
 		vim.keymap.set('n', '<leader>sr', builtin.resume, { desc = '[S]earch [R]esume' })
 		vim.keymap.set('n', '<leader>s.', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
 		vim.keymap.set('n', '<leader><leader>', builtin.buffers, { desc = '[ ] Find existing buffers' })
+		vim.keymap.set('n', '<leader>sf', builtin.lsp_document_symbols, { desc = 'Search functions in file' })
 
 		vim.keymap.set('n', '<leader>/', function()
 			builtin.current_buffer_fuzzy_find(require('telescope.themes').get_dropdown {
